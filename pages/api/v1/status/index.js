0435a5c050f713e44df15d3228a9bf0c9cdebfca
Alexsandro Teza Vilas Boas <alexteza@gmail.com>
2026-06-30 23:21:48 +0000

    finaliza o endpoint de `status` com informações do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 3+6;");
-  console.log(result);
-  response.status(200).json({ status: "OK" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionQuery = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionQuery.rows[0].server_version;
+
+  const databaseMaxConnectionsQuery = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsQuery.rows[0].max_connections;
+
+  const databaseOpenedConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsQuery.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
